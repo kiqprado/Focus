009c6b2b8558b97a2281e6b18969a107390179ee
Kaique Prado <kiqprado@outlook.com>
2023-09-06 12:08:16 -0300

    css fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './js/toggle.js'
 import './js/help/help.js'
 import * as FocusTimer from './js/FocusTimer/index.js'
-import './js/screenToggle.js'
+//import './js/screenToggle.js' // Deixando off.
 
 FocusTimer.start(30, 0)
